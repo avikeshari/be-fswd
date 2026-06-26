5f430566ba8a34765c7df59a7c56193f8285eb65
avikeshari <keshari.avinash7@gmail.com>
2026-06-27 03:19:10 +0530

    All operations of FS Module

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ fs.readFile('input.txt' , 'utf-8' , (err,data) => {
         console.log(data);
     }
 });
+fs.writeFile('output.txt' , 'Hello' , (err) => {
+    if(err) {
+        console.log("File Reading Error!");
+        return;
+    }
+});
+fs.appendFile('output.txt' , ' World' , (err) => {
+    if(err) {
+        console.log("File Reading Error!");
+        return;
+    }
+});
+fs.unlink('output.txt' , (err) => {
+    if(err) {
+        console.log("File Reading Error!");
+        console.log(err);
+        return;
+    }
+    else {
+        console.log("Deleted");
+    }
+});
