7150031af34f13687967e447308acf1bbe854c17
avikeshari <keshari.avinash7@gmail.com>
2026-06-27 12:52:16 +0530

    request-response cycle --response

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const http = require('http');
 const server = http.createServer((request,response) => {
+    response.setHeader('Content-Type', 'application/json');
+    //response.writeHead(201, {'Content-Type': 'text/plain'});
+    //response.writeHead(201, {'Content-Type': 'application/json'});
+    //response.writeHead(201, {'Content-Type': 'text/html'});
     response.write("Hello!");
+    /*response.write(
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>NODEJS</title>
+            </head>
+            <body>
+                <div>
+                    <h1>Hello!</h1>
+                    <p>Welcome!</p>
+                </div>
+            </body>
+        </html>
+    );*/
     response.end();
 });
 server.listen(3001,'localhost',() => {
