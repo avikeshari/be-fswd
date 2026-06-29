de691bc31534fa9648c7c8a3074c71bfa32e0e5a
avikeshari <keshari.avinash7@gmail.com>
2026-06-30 04:36:32 +0530

    server + app.js code split

--- server.js
+++ server.js
@@ -5,31 +5,7 @@ const server = http.createServer((request,response) => {
 server.listen(3001,'localhost',() => {
     console.log("Running");
 });*/
-const express = require('express');
-const logger = require('./middlewares/logger'); //Importing middleware
-const errorRoute = require('./middlewares/errorRoute');
-const app = express();
-//use middleware
-app.use(logger);
-app.get('/', (request, response) => {
-    response.json({message: 'Express GET'});
-});
-app.post('/', (request, response) => {
-    response.json({message: 'Express POST'});
-});
-app.put('/', (request, response) => {
-    response.json({message: 'Express PUT'});
-});
-app.patch('/', (request, response) => {
-    response.json({message: 'Express PATCH'});
-});
-app.delete('/', (request, response) => {
-    response.json({message: 'Express DELETE'});
-});
-app.get('/products', (request, response) => {
-    response.json({message: 'Express GET PRODUCTS'});
-});
-app.use(errorRoute);
+const app = require('./app');
 app.listen(3001, 'localhost', (error) => {
     if (error) {
         console.log('Error in running the server');
