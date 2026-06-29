fe9bdfd8119108d75dcbb0629ff0e864e1309159
avikeshari <keshari.avinash7@gmail.com>
2026-06-29 18:56:25 +0530

    Different Msgs for GET, PUT, POST, PATCH, DELETE

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 const http = require('http');
 const server = http.createServer((request,response) => {
-    response.setHeader('Content-Type', 'application/json');
+    const parseURL = new URL(request.url, `http://${request.headers.host}`);
+    const pathname = parseURL.pathname;
+    const method = request.method;
+
+    if(pathname === '/') {
+        if(method === 'GET') {
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify({message:'GET Root'}));
+            response.end();
+        }
+        else if(method === 'POST') {
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify({message:'POST Root'}));
+            response.end();
+        }
+        else if(method === 'PUT') {
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify({message:'PUT Root'}));
+            response.end();
+        }
+        else if(method === 'PATCH') {
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify({message:'PATCH Root'}));
+            response.end();
+        }
+        else if(method === 'DELETE') {
+            response.setHeader('Content-Type', 'application/json');
+            response.write(JSON.stringify({message:'DELETE Root'}));
+            response.end();
+        }
+    }
+    else if(pathname === '/users') {
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({message:'Users'}));
+        response.end();
+    }
+    else if(pathname === '/products') {
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({message:'Products'}));
+        response.end();
+    }
+    //response.setHeader('Content-Type', 'application/json');
     //response.writeHead(201, {'Content-Type': 'text/plain'});
     //response.writeHead(201, {'Content-Type': 'application/json'});
     //response.writeHead(201, {'Content-Type': 'text/html'});
-    response.write("Hello!");
+    //response.write("Hello!");
+    //response.write(JSON.stringify({message:'World!'}));
     /*response.write(
         <!DOCTYPE html>
         <html>
@@ -19,7 +61,7 @@ const server = http.createServer((request,response) => {
             </body>
         </html>
     );*/
-    response.end();
+    //response.end();
 });
 server.listen(3001,'localhost',() => {
     console.log("Running");
